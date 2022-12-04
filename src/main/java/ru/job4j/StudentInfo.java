8c7c6d2cf9d2ef370f77b9d1b3a5d8c720951027
nezhniy <nezhniystrastniy@yandex.ru>
2022-12-04 14:51:44 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Yaroslav");
+        System.out.println("06.01.2019");
+    }
+}
