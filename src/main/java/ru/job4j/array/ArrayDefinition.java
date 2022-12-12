f6ce072657afa597650c4e0938ea921de02f181d
nezhniy <nezhniystrastniy@yandex.ru>
2022-12-12 12:47:23 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,13 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Petr Antev";
+        names[2] = "Petr Arsev";
+        names[3] = "Petr Arentev";
+
+        System.out.println(names[0] + "\n" + names[1] + "\n" + names[2] + "\n" + names[3]);
     }
 }
