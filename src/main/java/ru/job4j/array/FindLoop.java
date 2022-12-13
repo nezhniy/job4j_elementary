ddb14a3150a19df45f8ded70493f00868974c925
nezhniy <nezhniystrastniy@yandex.ru>
2022-12-13 10:56:51 +0500

    6.1. Классический поиск перебором. [#33489 #370298]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 rst = i;
@@ -15,7 +14,6 @@ public class FindLoop {
 
     public static int indexInRange(int[] data, int el, int start, int finish) {
         int rst = -1;
-
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
