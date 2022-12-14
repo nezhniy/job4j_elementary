baabe5d4349f1a8eec6a4fd38c66020687315f7c
nezhniy <nezhniystrastniy@yandex.ru>
2022-12-14 14:31:00 +0500

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (name.isEmpty() || isLowerLatinLetter(name.codePointAt(0))) {
+            for (int i = 1; i < name.length(); i++) {
+                if (isLowerLatinLetter(name.codePointAt(i))
+                    || isSpecialSymbol(name.codePointAt(i))
+                    || isUpperLatinLetter(name.codePointAt(i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
